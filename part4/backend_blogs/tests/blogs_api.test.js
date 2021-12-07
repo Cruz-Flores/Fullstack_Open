2ffb2744055ceebde372a20b3a2ac48a14121d25
Cruz <cruz.tag@gmail.com>
2021-12-06 22:30:55 -0600

    added module helper and refactorized

--- part4/backend_blogs/tests/blogs_api.test.js
+++ part4/backend_blogs/tests/blogs_api.test.js
@@ -1,52 +1,15 @@
-const mongoose = require('mongoose');
 const supertest = require('supertest');
+const mongoose = require('mongoose');
+const helper = require('./test_helper.js');
 const app = require('../app.js');
 const api = supertest(app);
 const Blog = require('../models/blog.js');
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  },
-  {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-  },
-  {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-  },
-  {
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-  },
-  {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-  },
-];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = new Blog(initialBlogs[0]);
+  let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
-  blogObject = new Blog(initialBlogs[1]);
+  blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
 });
 
@@ -58,7 +21,7 @@ test('blogs are returned as jason', async () => {
 
   const response = await api.get('/api/blogs');
 
-  expect(response.body).toHaveLength(initialBlogs.length - 4);
+  expect(response.body).toHaveLength(helper.initialBlogs.length - 4);
 });
 
 test('id checker', async () => {
@@ -83,7 +46,7 @@ test('a blog can be added', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await Blog.find({});
+  const blogsAtEnd = await helper.findBlogs();
   expect(blogsAtEnd).toHaveLength(3);
 });
 
@@ -99,7 +62,7 @@ test('a blog without id set default 0', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await Blog.find({});
+  const blogsAtEnd = await helper.findBlogs();
   expect(blogsAtEnd[2].likes).toBe(0);
 });
 
