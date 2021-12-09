bbe585672e420366c8d0dfbb61d043d9b2e9655e
Cruz <cruz.tag@gmail.com>
2021-12-08 20:48:36 -0600

    modified path post for add user info to the blog

--- part4/backend_blogs/controllers/blogs.js
+++ part4/backend_blogs/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog.js');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
@@ -9,14 +10,20 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id,
   });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedNote._id);
+  await user.save();
+
   response.json(savedBlog);
 });
 
