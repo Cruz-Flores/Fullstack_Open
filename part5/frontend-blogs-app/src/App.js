988c27c505e4f45c6700cf120d743f7bde81f531
Cruz <cruz.tag@gmail.com>
2021-12-12 22:03:21 -0600

    first commit

---
+++ part5/frontend-blogs-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog.js';
+import blogsService from './services/blogs.js';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    blogsService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ part5/frontend-blogs-app/src/services/blogs.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/api/blogs';
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const blogsService = {
+  getAll,
+};
+
+export default blogsService;
